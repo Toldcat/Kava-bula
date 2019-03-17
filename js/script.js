e8bb0e729a65876a03b9b7caf68b281046637773
Toldcat <Andrew.avd@gmail.com>
2019-03-17 00:52:11 +0000

    added the project files to gitHub

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    autoplay: true,
+    mouseDrag: false,
+    autoplayTimeout: 8000
+  })
+});
